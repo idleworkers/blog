fdd27a74c2d3deeb72b14c7f38d2b712b70462b4
Mohamed Hayibor <mohamedhayibor@gmail.com>
2020-01-30 02:21:22 -0500

    Add seo improvs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,20 @@
 module.exports = {
   plugins: [
+   {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Idle Workers Research and Development blog`,
+        short_name: `IdleWorkers`,
+        start_url: `/`,
+        font_size: `40px`,
+        // background_color: `#f7f0eb`,
+        // theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-theme-blog`,
       options: {},
